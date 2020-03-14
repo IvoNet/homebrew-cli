e94ebcc4fe357f5256fc1d9f0d2b61950d14d628
Ivo Woltring <ivo@ivonet.nl>
2020-03-14 16:33:15 +0100

    test commit

--- Formula/gcd.rb
+++ Formula/gcd.rb
@@ -22,6 +22,6 @@ class Gcd < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "gcd"
+    system "true"
   end
 end
