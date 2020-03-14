a56505264585fc1fedf681e0b696a018878373f4
Ivo Woltring <ivo@ivonet.nl>
2020-03-14 19:23:45 +0100

    made formula work

--- Formula/menv.rb
+++ Formula/menv.rb
@@ -3,28 +3,15 @@
 class Menv < Formula
   desc "menv - The environment changer for maven"
   homepage "https://www.ivonet.nl"
-  url "https://github.com/IvoNet/menv/archive/v0.7.tar.gz"
+  url "https://github.com/IvoNet/menv/archive/v0.8.tar.gz"
   version "0.6"
-  # sha256 "bd5099adac722b9784f49313da0c5d09946b3f222890594f0be9b1bdeefcb294"
+  sha256 "25584961def504a9fe4c016412e9860d48f306cc2cfd4733de8187d85f1af948"
 
   depends_on "maven"
 
   def install
     bin.install "bin/menv"
     libexec.install Dir["*"]
-    # bin.write_exec_script libexec/"bin/mvn"
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test menv`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "menv", "-h"
-  end
+  
 end
