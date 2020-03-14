3001303f6d08f4e62664a49e63ff8cf797f6a20e
Ivo Woltring <ivo@ivonet.nl>
2020-03-14 23:10:13 +0100

    added ivo2u

--- Formula/ivo2u.rb
+++ Formula/ivo2u.rb
@@ -1,17 +1,15 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Ivo2u < Formula
-  desc "ivo2u - the ivo tiny url service"
-  homepage "https://www.ivonet.nl"
-  url "https://github.com/IvoNet/menv/archive/v0.8.tar.gz"
-  version "0.8"
+  desc "ivo2u - The Ivo2U tiny url service"
+  homepage "http://ivo2u.nl"
+  url "https://github.com/IvoNet/homebrew-ivonet/archive/master.zip"
+  version "0.1"
   sha256 "25584961def504a9fe4c016412e9860d48f306cc2cfd4733de8187d85f1af948"
 
-  depends_on "maven"
 
   def install
-    bin.install "bin/menv"
-    libexec.install Dir["*"]
+    bin.install "bin/ivo2u"
   end
   
 end
