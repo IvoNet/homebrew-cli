7103f84019c322d8589709f20c018442f00e3465
Ivo Woltring <ivo@ivonet.nl>
2020-03-14 23:15:02 +0100

    Added some scripts

--- Formula/ivonet-cli.rb
+++ Formula/ivonet-cli.rb
@@ -1,8 +1,8 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-class Ivo2u < Formula
-  desc "ivo2u - The Ivo2U tiny url service"
-  homepage "http://ivo2u.nl"
+class IvoNetCli < Formula
+  desc "IvoNetCLI - IvoNet CLI tools."
+  homepage "https://github.com/IvoNet/homebrew-ivonet/tree/master/bin"
   url "https://github.com/IvoNet/homebrew-ivonet/archive/master.zip"
   version "0.1"
   sha256 "885526b94403ff415facf8dc4f2cb523d56226994e5b20867bc1be6441b5a48c"
@@ -10,6 +10,10 @@ class Ivo2u < Formula
 
   def install
     bin.install "bin/ivo2u"
+    bin.install "bin/m4b"
+    bin.install "bin/docker-project"
+    bin.install "bin/docker-ui-project"
+    bin.install "bin/drawio"
   end
   
 end
