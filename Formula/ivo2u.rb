09853358c97e3834dee3460f286d5cec3ec6e0d1
Ivo Woltring <ivo@ivonet.nl>
2020-03-15 09:53:07 +0100

    renamed to new repo name

--- Formula/ivo2u.rb
+++ Formula/ivo2u.rb
@@ -4,7 +4,7 @@ class Ivo2u < Formula
   desc "ivo2u - The Ivo2U tiny url service"
   homepage "http://ivo2u.nl"
   url "https://github.com/IvoNet/homebrew-cli/archive/v0.2.tar.gz"
-  version "0.1"
+  version "0.2"
   sha256 "cf2668de6508aa87db95fa53a08eca78422c9899b719418e65e4bead55ded3fe"
 
 
