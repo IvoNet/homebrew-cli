6688176cad3cbf42f46cf1991e34bad9098b5bb5
Ivo Woltring <ivo@ivonet.nl>
2020-03-15 11:37:01 +0100

    sha

--- Formula/ivonet-common.rb
+++ Formula/ivonet-common.rb
@@ -5,7 +5,7 @@ class IvonetCommon < Formula
   homepage "https://github.com/IvoNet/homebrew-ivonet/tree/master/bin"
   url "https://github.com/IvoNet/homebrew-cli/archive/v0.4.tar.gz"
   version "0.1"
-  sha256 "bc0a3836a9fb1d5422f51245a12d0c5d19b8294bc9d0dc34d606ada630d9e35e"
+  sha256 "4fff1f4e87929fd8a43470e032a9d9f39b4c35f08f18cde4a5a51e30ad3506ea"
 
   def install
     bin.install "bin/histg"
