633242cd551d16f29b7d2bda86294cac3b0acac8
Ivo Woltring <ivo@ivonet.nl>
2020-03-15 11:40:54 +0100

    updated formulae

--- Formula/ivonet-common.rb
+++ Formula/ivonet-common.rb
@@ -3,7 +3,7 @@
 class IvonetCommon < Formula
   desc "IvoNet Common CLI tools."
   homepage "https://github.com/IvoNet/homebrew-ivonet/tree/master/bin"
-  url "https://github.com/IvoNet/homebrew-cli/archive/v0.4.tar.gz"
+  url "https://github.com/IvoNet/homebrew-cli/archive/v0.5.tar.gz"
   version "0.1"
   sha256 "4fff1f4e87929fd8a43470e032a9d9f39b4c35f08f18cde4a5a51e30ad3506ea"
 
