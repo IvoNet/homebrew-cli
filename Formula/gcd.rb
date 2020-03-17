d02e0b3f699dc602d7a6885d429a4bbd3311fa71
Ivo Woltring <ivo@ivonet.nl>
2020-03-17 20:56:16 +0100

    v0.7 of git-cd

--- Formula/gcd.rb
+++ Formula/gcd.rb
@@ -3,35 +3,41 @@
 class Gcd < Formula
   desc "gcd - The git change directory tool"
   homepage "https://www.ivonet.nl"
-  url "https://github.com/IvoNet/git-cd/archive/0.1.tar.gz"
-  version "0.1"
-  sha256 "142a2b678a9033932dae69c7604286a933263d6edb069d27243ab53c303c3459"
+  url "https://github.com/IvoNet/git-cd/archive/v0.7.tar.gz"
+  version "0.7"
   head "https://github.com/IvoNet/git-cd.git"
+  sha256 "a837cf313ef746541e6939f434cecad086ec56df8becb9185dbe01d8acef978e"
 
   depends_on "python@3"
 
   def install
     bin.install "bin/gcd"
+    bin.install "bin/cdi"
     libexec.install Dir["*"]
   end
 
   def caveats
     <<~EOS
-        Add the following line to your .bashrc or equivalent:
-          alias gcd="#{bin}"
+##############################################################################
+#  NOTE!!                                                                    #
+##############################################################################
+#  Add the following lines to your .zshrc / .bashrc or equivalent:           #
+##############################################################################
+   alias gcd="source #{HOMEBREW_PREFIX}/bin/gcd"         
+   alias gcdrescan="rm -f \${HOME}/.gcd/gcd.cache 2>/dev/null && gcd"  
+   alias gcdreset="rm -f \${HOME}/.gcd/gcd.sqlite 2>/dev/null && gcd"
+   alias cdi="source #{HOMEBREW_PREFIX}/bin/cdi"  
+##############################################################################
+#  If you want to change the number of favorites to display                  #
+#  add the following with a number (default 10):                             #
+##############################################################################
+   export GCD_FAVORITES=20
+##############################################################################
+#    Known issues:                                                           #
+#    - Paths with spaces in them will have the spaces in the menu replaced   #
+#      with the ? symbol to make them more readable                          #
+##############################################################################
     EOS
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test menv`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
-  end
 end
