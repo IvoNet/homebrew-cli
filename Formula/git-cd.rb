069df8b13a3b94568332e835d1dbca1e49bb069f
Ivo Woltring <ivo@ivonet.nl>
2020-03-17 21:19:06 +0100

    v0.9 of git-cd

--- Formula/git-cd.rb
+++ Formula/git-cd.rb
@@ -3,14 +3,15 @@
 class GitCd < Formula
   desc "gcd - The git change directory tool"
   homepage "https://www.ivonet.nl"
-  url "https://github.com/IvoNet/git-cd/archive/v0.7.tar.gz"
-  version "0.7"
+  url "https://github.com/IvoNet/git-cd/archive/v0.9.tar.gz"
+  version "0.9"
   head "https://github.com/IvoNet/git-cd.git"
-  sha256 "a837cf313ef746541e6939f434cecad086ec56df8becb9185dbe01d8acef978e"
+  sha256 "79e2bb4859bfcc151e975bc81bd7e9d17a559735d60c3f2236ba8a31111ac746"
 
   depends_on "python@3"
 
   def install
+    inreplace "bin/gcd", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
     bin.install "bin/gcd"
     bin.install "bin/cdi"
     libexec.install Dir["*"]
