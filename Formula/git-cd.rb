587f5df5aa18b219205e617f23aac6b0fae48742
Ivo Woltring <ivo@ivonet.nl>
2020-03-20 00:54:43 +0100

    Added more functionality to git-cd

--- Formula/git-cd.rb
+++ Formula/git-cd.rb
@@ -3,22 +3,43 @@
 class GitCd < Formula
   desc "gcd - The git change directory tool"
   homepage "https://github.com/IvoNet/git-cd"
-  url "https://github.com/IvoNet/git-cd/archive/v0.14.tar.gz"
-  version "0.14"
+  url "https://github.com/IvoNet/git-cd/archive/v0.15.tar.gz"
+  version "0.15"
   head "https://github.com/IvoNet/git-cd.git"
-  sha256 "910866b5820d5a0c29b41e90890d708c6dba8c069e4c0df1d59a3c0944bd8870"
+  sha256 "41e21342196c735ada2cbd9b86c1d92c8c94362925fe7730da54f4292506f6fd"
 
   depends_on "python@3"
+  depends_on "coreutils"
 
   def install
+    inreplace "bin/ccd", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/ccd"
+    
+    inreplace "bin/ccd-help", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/ccd-help"
+
     inreplace "bin/gcd", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
-    inreplace "bin/gcdcron", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
-    inreplace "bin/gcdrescan", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
-    inreplace "bin/gcdresetmetrics", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
     bin.install "bin/gcd"
+
     bin.install "bin/cdi"
-    bin.install "bin/gcdrescan"
-    bin.install "bin/gcdresetmetrics"
+    
+    inreplace "bin/gcd-alias", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/gcd-alias"
+
+    inreplace "bin/gcd-rescan", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/gcd-rescan"
+
+    inreplace "bin/gcd-reset-all", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/gcd-reset-all"
+
+    inreplace "bin/gcd-unalias", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/gcd-unalias"
+
+    inreplace "bin/gcd-zap", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/gcd-zap"
+    
+    inreplace "bin/gcdcron", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    
     libexec.install Dir["*"]
   end
 
@@ -28,11 +49,12 @@ class GitCd < Formula
 # git-cd (gcd) will scan and index all your git projects in your HOME 
 # folder and below for easy switching between them.
 #############################################################################
-# NOTE!!                                                                    
+# NOTE!! Read the following carefully                                                                    
 #############################################################################
 # Add the following line to your .zshrc / .bashrc or equivalent:           
 #############################################################################
 alias gcd="source #{HOMEBREW_PREFIX}/bin/gcd"
+alias ccd="source #{HOMEBREW_PREFIX}/bin/ccd"
 # if you develop with IntelliJ idea and you have the commandline tool
 # this will navigate to the folder and open intellij there :-)         
 alias cdi="source #{HOMEBREW_PREFIX}/bin/cdi" 
@@ -58,6 +80,8 @@ export GCD_PROJECTS_DIR=<your_directory_here>
 0 8 * * * #{opt_libexec}/bin/gcdcron  
 #  This one every day at 7 / 12 / 15 / 20 hours
 0 7,12,15,20 * * * #{opt_libexec}/bin/gcdcron  
+#############################################################################
+# Full guide `brew home git-cd`
 #############################################################################
 # Known issues:                                                           
 # - Paths with spaces in them will have the spaces in the menu replaced   
