c358313824ef951f001c00a90035871d19e3dbb6
Woltring, I.A.H. (Ivo) <Ivo.Woltring@devolksbank.nl>
2020-03-20 12:20:52 +0100

    v0.19 - beter documentation - configurable cdi

--- Formula/git-cd.rb
+++ Formula/git-cd.rb
@@ -59,9 +59,9 @@ class GitCd < Formula
 alias gcd="source #{HOMEBREW_PREFIX}/bin/gcd"
 alias ccd="source #{HOMEBREW_PREFIX}/bin/ccd"
 alias cdi="source #{HOMEBREW_PREFIX}/bin/cdi" 
-# ^^^ cdi - to open an IDE in the folder you navigate to (optional)
-#     add `export GCD_EXEC=<path_to_your_editor_here>` to your .zshrc
-#     defaults to `idea` (IntelliJ)         
+# ^^^ cdi - to execute a command by default in the folder you navigate to
+#     add `export GCD_EXEC="<your_command_here>"` to your .zshrc
+#     defaults to `idea .` (IntelliJ)         
 #############################################################################
 # Full guide `brew home git-cd` as there are many more options and commands
 # available in this package like: ccd, ccd-alias, gcd-rescan, etc...
