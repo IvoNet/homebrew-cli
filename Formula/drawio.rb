3bb78a31ec9b66fb970daab700f710ce9fd8c122
Ivo Woltring <ivo@ivonet.nl>
2020-03-31 22:03:46 +0200

    v0.12

--- Formula/drawio.rb
+++ Formula/drawio.rb
@@ -3,9 +3,9 @@
 class Drawio < Formula
   desc "IvoNetCLI - IvoNet CLI tools."
   homepage "https://github.com/IvoNet/homebrew-ivonet/tree/master/bin"
-  url "https://github.com/IvoNet/homebrew-cli/archive/v0.2.tar.gz"
-  version "0.2"
-  sha256 "bc0a3836a9fb1d5422f51245a12d0c5d19b8294bc9d0dc34d606ada630d9e35e"
+  url "https://github.com/IvoNet/homebrew-cli/archive/v0.12.tar.gz"
+  version "0.12"
+  sha256 "377e2d6a5f47878261e2bd7fe46285128cbb413f43c72d522069858a6abce2b9"
 
   def install
     bin.install "bin/drawio"
