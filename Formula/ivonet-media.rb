ab9ecd93ad3701f4e3045c800bb6e16a1cf3b260
Ivo Woltring <ivo@ivonet.nl>
2020-04-27 20:01:16 +0200

    v0.16

--- Formula/ivonet-media.rb
+++ Formula/ivonet-media.rb
@@ -3,9 +3,9 @@
 class IvonetMedia < Formula
   desc "IvoNetMedia - IvoNet Media CLI tools."
   homepage "https://github.com/IvoNet/homebrew-ivonet/"
-  url "https://github.com/IvoNet/homebrew-cli/archive/v0.15.tar.gz"
-  version "0.15"
-  sha256 "fe1b12e489f6d78952ad3449c914431c091c2f85708fce6439b3728884027c86"
+  url "https://github.com/IvoNet/homebrew-cli/archive/v0.16.tar.gz"
+  version "0.16"
+  sha256 "a42377b5157d0fa5152be9fb8646daeca106a6ecac94c3dc90e549658b50d186"
 
   def install
     bin.install "bin/m4b"
