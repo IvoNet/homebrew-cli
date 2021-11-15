9733a2b94568c52670e5331610973c2bb6aa6e80
Ivo Woltring <ivo@ivonet.nl>
2021-11-15 10:45:23 +0100

    Updated git-cd to v1.7 - fixed gcd-status bug

--- Formula/git-cd.rb
+++ Formula/git-cd.rb
@@ -29,6 +29,9 @@ class GitCd < Formula
 
     inreplace "bin/gcd-add", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
     bin.install "bin/gcd-add"
+    
+    inreplace "bin/gcd-status", "!!GCDLIBEXECBIN!!", "#{opt_libexec}/bin"
+    bin.install "bin/gcd-status"
 
     bin.install "bin/cdi"
     bin.install "bin/cdc"
